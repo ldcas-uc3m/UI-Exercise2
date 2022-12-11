a798914c90863ed1a0ff1afdac7a881a12e9964b
laumardi <100428953@alumnos.uc3m.es>
2022-12-11 21:02:46 +0100

    fixed podcast

--- script/podcast-upload.js
+++ script/podcast-upload.js
@@ -15,7 +15,7 @@ image_input.addEventListener("change", function() {
 
 function createPodcast(){
     let def_pic = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9398c530-d0a7-4c46-99b4-423a6aabf39f/d3kxnbe-f16dabfb-0cf1-436c-9315-915fbe462f23.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzkzOThjNTMwLWQwYTctNGM0Ni05OWI0LTQyM2E2YWFiZjM5ZlwvZDNreG5iZS1mMTZkYWJmYi0wY2YxLTQzNmMtOTMxNS05MTVmYmU0NjJmMjMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Gk0TAQTWUZBrHFjRC0y7pMEEdep95g7p6upVZNpXaZg"
-    let play_pict = document.getElementById("image_input").value;
+    let play_pict = document.getElementById("pict").value;
     let play_name = document.getElementById("title").value;
     if (play_name == ""){
         let output = "Podcast must have a name";
